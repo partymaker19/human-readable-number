9714c74bbde685476bc7f0325bfae9f186a57312
Ilya <82887965+partymaker19@users.noreply.github.com>
2023-01-12 23:11:16 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const num = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const ten = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    const hundred = [
+        "one hundred",
+        "two hundred",
+        "three hundred",
+        "four hundred",
+        "five hundred",
+        "six hundred",
+        "seven hundred",
+        "eight hundred",
+        "nine hundred",
+    ];
+
+    if (number < 20) {
+        return num[number];
+    } else if (number < 100) {
+        return (
+            ten[Math.floor(number / 10 - 2)] +
+            (number % 10 ? " " + num[number % 10] : "")
+        );
+    } else if (number < 1000) {
+        return (
+            hundred[Math.floor(number / 100 - 1)] +
+            (number % 100 ? " " + toReadable(number % 100) : "")
+        );
+    }
+};
